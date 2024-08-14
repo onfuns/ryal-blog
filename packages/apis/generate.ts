bde42bcd4a0257ada552e786a07dc5d69b33fdb9
onfuns <wanrenya@gmail.com>
2024-08-14 19:16:12 +0800

    feat: 生成 api

--- packages/apis/generate.ts
+++ packages/apis/generate.ts
@@ -1,10 +1,11 @@
+import fs from 'node:fs/promises'
 import path from 'path'
 import { generateApi } from 'swagger-typescript-api'
 import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 generateApi({
-  output: path.join(__dirname, './src/api'),
+  output: path.join(__dirname, './src'),
   url: 'http://localhost:4000/swagger/api-docs.json',
   httpClientType: 'axios',
   modular: true,
@@ -15,4 +16,10 @@ generateApi({
       return templateRouteName.replace('Controller', '')
     },
   },
+}).then(({ files }) => {
+  let content = ''
+  files.forEach(file => {
+    content += `export * from './src/${file.fileName}${file.fileExtension}'\n`
+    fs.writeFile(path.join(__dirname, './index.ts'), content, 'utf-8')
+  })
 })
---
+++ packages/apis/index.ts
@@ -0,0 +1,4 @@
+export * from './src/Api.ts'
+export * from './src/Article.ts'
+export * from './src/data-contracts.ts'
+export * from './src/http-client.ts'
